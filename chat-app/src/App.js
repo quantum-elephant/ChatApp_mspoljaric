450b7a9b327995ff5f219da109c6c5a8725c33cc
quantum-elephant <69718885+quantum-elephant@users.noreply.github.com>
2024-01-29 11:37:33 +0100

    initiated
    initiated

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,5 +1,21 @@
+import { useState } from 'react';
 import './Styles/App.scss';
 
 export default function App() {
-  return;
+  const messageArr = [
+    'Hello',
+    'Goodbye',
+    'I am a friend',
+    'I love potato',
+    'I am learning WebDev',
+  ];
+  const [messages, setMessages] = useState(messageArr);
+
+  return (
+    <>
+      {messages.map((message) => (
+        <li key={crypto.randomUUID}>{message}</li>
+      ))}
+    </>
+  );
 }
