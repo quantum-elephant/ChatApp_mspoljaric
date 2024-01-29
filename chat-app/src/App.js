6c364c6c7b2c6f9210f5bf7cbaea63159250c3be
quantum-elephant <69718885+quantum-elephant@users.noreply.github.com>
2024-01-29 10:57:40 +0100

    restart

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,25 +1,5 @@
-import { useState } from 'react';
-import './App.css';
-import ShowMessages from './Components/ShowMessages';
+import './Styles/App.scss';
 
 export default function App() {
-  const [messages, setMessages] = useState([
-    {
-      id: '1',
-      data: 'This is a test message!',
-      member: {
-        id: '1',
-        clientData: {
-          color: 'blue',
-          username: 'bluemoon',
-        },
-      },
-    },
-  ]);
-
-  return (
-    <div className='App'>
-      <ShowMessages messages={messages} />
-    </div>
-  );
+  return;
 }
